80179d523c4fcbc0e6fc724230251b7d59293fdf
AgustinGaitan <agusgaitan2009@hotmail.com>
2021-03-23 19:19:30 -0300

    asd

---
+++ clase01/guia/ej06/ej6.js
@@ -0,0 +1,11 @@
+"use strict";
+var cubo = function (x) {
+    return x * x * x;
+};
+var mostrarCubo = function (x) {
+    console.log(cubo(x));
+};
+mostrarCubo(5);
+mostrarCubo(3);
+mostrarCubo(10);
+//# sourceMappingURL=ej6.js.map
---
+++ clase01/guia/ej06/ej6.ts
@@ -0,0 +1,18 @@
+
+var cubo = (x : number) : number =>{
+
+
+    return x * x * x;
+
+}
+
+var mostrarCubo = (x:number) : void =>{
+
+
+    console.log(cubo(x));
+
+}
+
+mostrarCubo(5);
+mostrarCubo(3);
+mostrarCubo(10);
